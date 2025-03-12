19723879f902679a6bc2c517c6238c487e2578a4
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:04:27 +0700

    menambahkan class kode produk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul4_103022300035;
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Daftar kode produk: ");
+        Console.WriteLine("------------------------------------");
+
+        string[] daftarProduk =
+        {
+            "Laptop", "Smartphone", "Tablet", "Headset", "Keyboard",
+            "Mouse", "Printer", "Monitor", "Smartwatch", "Kamera"
+        };
+
+        foreach (string produk in daftarProduk)
+        {
+            Console.WriteLine($"{produk}: {KodeProduk.getKodeProduk(produk)}");
+        }
+    }
+}
